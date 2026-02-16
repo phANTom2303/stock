a159a1d5f0570885e0546bc5366be097a180e185
jaspreetjk20 <jaspreetsk.2020@gmail.com>
2026-02-16 18:31:03 +0530

    Get company_list feature added

---
+++ server/controllers/companyList.js
@@ -0,0 +1,39 @@
+const pool = require('../db')
+
+async function handleGetCompanyList(req, res){
+    try{
+        //Fetch from db
+        const query = `SELECT company_symbol, company_name
+                       FROM companies
+                       ORDER BY company_name ASC`;
+        
+        //Get result
+        const result = await pool.query(query);
+
+        //Check if company name exists in db
+        if (result.rows.length === 0){
+            return res.status(404).json({
+                success: false,
+                message: "No companies available"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            companies: result.rows
+        });
+    }
+
+    catch(error){
+        console.error("Error:", error);
+        return res.status(500).json({
+        success: false,
+        message: "Server error",
+    });
+    }  
+}
+
+module.exports = {
+    handleGetCompanyList,
+};
+
